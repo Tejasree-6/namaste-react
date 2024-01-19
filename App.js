388e142ed9d5a3c59650ed4b01a500441b23ec05
Tejasree <tejasree6633@gmail.com>
2024-01-19 18:30:20 -0500

    creating multiple divs

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
-const heading=React.createElement("h1",{},"Hello World from React");
+const heading=React.createElement(
+    "div",
+    {id:"parent"},
+   React.createElement(
+    "div",
+    {id:"child"},
+    React.createElement(
+        "h1",
+        {},
+        "I am inside Div")
+    )
+);
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
