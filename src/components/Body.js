5dc3da56ed2e75747a4ad1907be1f0f57d2501a2
Tejasree <tejasree6633@gmail.com>
2024-02-04 22:11:31 -0500

    shimmer ui, input, onChange and filtered cards on ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import ResCard from "./ResCard";
 import { stores } from "../utils/mockData";
 import ShimmerUI from "./ShimmerUI";
+import PageNotFound from "./PageNotFound";
 
  
 
@@ -9,25 +10,40 @@ import ShimmerUI from "./ShimmerUI";
 const Body=()=>{
   const [dataList,setDataList]=useState(stores);
   let [apiData,setApiData]=useState([]);
+  let [filteredData,setFilteredData]=useState([]);
+  const [searchText, setSearchText]=useState("");
 
    useEffect(()=>{
       fetchData();
    },[])
-
+  
    const fetchData= async()=>{
       let data= await fetch("https://dummyjson.com/recipes");
      
       data=await data.json();
       setApiData(data?.recipes);
+      setFilteredData(data?.recipes);
    }
-   console.log(apiData);
-  console.log(dataList);
-  if(apiData.length===0){
-    return <ShimmerUI/>
-  }
-    return (
+  // console.log("api data from use effect",apiData)
+  console.log("Body rendered")
+    return apiData.length===0? <ShimmerUI/>:(
       <div className='body'>
           <div className='filter'> 
+          <div className="search">
+              <input type="text" className="search-box" value={searchText} onChange={(e)=>{
+                    setSearchText(e.target.value);
+              }}/>
+              <button onClick={()=>{
+           
+              const filteredData1=  apiData.filter((data)=>data.name.toLowerCase().includes(searchText.toLocaleLowerCase()));
+              //  setApiData(filteredData);
+                {if(filteredData1.length==0){
+                  console.log("page not found");
+                  return <ShimmerUI/>;
+                }else setFilteredData(filteredData1)}
+                console.log(filteredData1)
+              }}>Search</button>
+          </div>
           <button className="filter-btn" 
           onClick={()=>{
            let dataList1=apiData.filter((data)=>data?.rating>4.5);
@@ -39,7 +55,7 @@ const Body=()=>{
           </button>
           </div>
           <div className='res-container'>
-           {apiData.map((resData)=>
+           {filteredData.map((resData)=>
            <ResCard key={resData.id} restaData={resData}
            
            />)}
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,9 @@
+const PageNotFound=()=>{
+    return(
+        <div className="page-not-found-container">
+            <h4>We didn't find a match</h4>
+            <p>Try seaching for something else instead</p>
+        </div>
+    )
+}
+export default PageNotFound;
