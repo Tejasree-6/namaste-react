3c157d6fe5793033c43a8c3dbe4f00eb2d33feeb
Tejasree <tejasree6633@gmail.com>
2024-02-05 13:57:04 -0500

    react-router-dom,createBrowserRouter,RouterProvider, routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import About from "./components/About";
+import ContactUs from "./components/ContactUs";
+import PageNotFound from "./components/PageNotFound";
+import Error from "./components/Error";
 
 
 const AppLayout = () => {
@@ -14,5 +18,24 @@ const AppLayout = () => {
     </div>
   );
 };
+const appRouter=createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>,
+    errorElement:<Error/>
+  },
+  {
+    path:"/about",
+    element:<About/>
+  },
+  {
+    path:"/contact",
+    element:<ContactUs/>
+  },
+  {
+    path:"/pagenotfound",
+    element:<PageNotFound/>
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter}/>);
---
+++ src/components/ContactUs.js
@@ -0,0 +1,9 @@
+const ContactUs=()=>{
+    return(
+        <div>
+            <h1>Contact Details</h1>
+            <p>tejasree6633@gmail.com</p>
+        </div>
+    )
+}
+export default ContactUs;
